325d4348498b101514f47145ed297062b5a01a29
aaabo <767324214@qq.com>
2021-10-30 17:57:42 +0800

    习题标签

--- src/components/User/ExerciseList.vue
+++ src/components/User/ExerciseList.vue
@@ -7,6 +7,18 @@
       class="handle-input"
     >
     </el-input>
+    <button @click="wwChange()">标签</button>
+    <div v-if="ww" class="label">
+      <div v-for="(i,o) in options" :key="o" style="float:left">
+        <span style="margin-right:500px">
+          {{i.label}}
+        </span>
+          <span v-for="(w,e) in options[o].children" :key="e" style="color:blue;cursor:pointer;" @click="labelClick(w.label)">
+              {{w.label}}
+          </span>
+      </div>
+      
+    </div>
     <el-table :data="data" style="width: 90%" class="tableclass" stripe>
       <el-table-column prop="exerciseId" label="习题编号"> </el-table-column>
       <el-table-column label="习题标题">
@@ -72,6 +84,9 @@ export default {
       currentPage: 1,
       isPublish:false,
       selectBackWord:'',
+      options:[],
+      ww:false,
+      labelChoice:[],
     }
   },
   watch: {
@@ -112,14 +127,73 @@ export default {
           sessionStorage.setItem("isPublish","false");
       }
     
+     //获取习题信息
     this.getExercise()
+    //获取习题标签信息
+    this.getFirstPointInfo()
      
   },
 
   
 
   methods: {
+    //点击标签
+    labelClick(secondPoint){
+      var i=0;
+      for(var o=0;o<this.labelChoice.length;o++){
+        if(this.labelChoice[o].secondPoint==secondPoint){
+          this.labelChoice.splice(o,1);
+          i=1;
+        }
+      }
+      if(i==0){
+        var label=new Object;
+        label.secondPoint=secondPoint;
+        this.labelChoice.push(label);
+      }
+      if(this.labelChoice.length==0){
+          let params = new URLSearchParams();
+          this.$axios({
+            method: 'post',
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded"
+            },
+            url: '/exercise/queryExerciseInfo',
+            data: params
+          })
+            .then((res) => {
+              this.selectExercise = res.data;
+              this.exercise = res.data;
+            })
+            .catch((err) => {
+              this.$message.error('习题列表加载失败');
+
+            })
+      }else{
+        let params = new URLSearchParams();
+        params.append("labels",JSON.stringify(this.labelChoice))
+        this.$axios({
+          method: 'post',
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+          },
+          url: '/exercise/queryExerciseInfoByFirstPoint',
+          data: params
+        })
+          .then((res) => {
+              this.selectExercise = res.data;
+              this.exercise = res.data;
+          })
+          .catch((err) => {
+            this.$message.error('习题标签加载失败');
+
+          })
+      }
       
+    },
+      wwChange(){
+        this.ww=!this.ww;
+      },
     //给sessionStorage存值
     setContextData: function(key, value) { 
       if(typeof value == "string"){
@@ -176,6 +250,57 @@ export default {
 
         })
     },
+    //获取第一点
+    getFirstPointInfo () {
+      let params = new URLSearchParams();
+      this.$axios({
+        method: 'post',
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        url: '/label/queryLabelFirstPointInfo',
+        data: params
+      })
+        .then((res) => {
+          for (var i = 0; i < res.data.length; i++) {
+            var firstPoint = new Object;
+            firstPoint.label = res.data[i];
+            firstPoint.children = [];
+            this.options.push(firstPoint);
+          }
+          for (var i = 0; i < this.options.length; i++) {
+            this.getSecondPointInfo(i);
+          }
+        })
+        .catch((err) => {
+          this.$message.error('失败1');
+        })
+    },
+    //获取第二点
+    getSecondPointInfo (i) {
+      let params = new URLSearchParams();
+      params.append("firstPoint", this.options[i].label)
+      this.$axios({
+        method: 'post',
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },
+        url: '/label/queryLabelSecondPointInfo',
+        data: params
+      })
+        .then((res) => {
+          for (var o = 0; o < res.data.length; o++) {
+            var secondPoint = new Object;
+            secondPoint.label = res.data[o].secondPoint;
+            secondPoint.value = res.data[o].labelId;
+            this.options[i].children.push(secondPoint);
+          }
+        })
+        .catch((err) => {
+          this.$message.error('失败2');
+        })
+    }
+    
   }
 }
 </script>
@@ -192,5 +317,13 @@ export default {
   width: 300px;
   display: inline-block;
   margin-left: 5%;
+}
+.label{
+   width:500px;
+   height:500px;
+  position:absolute;
+  top:120px;background:white;
+  z-index:999;
+  border-radius: 30px
 }
 </style>
